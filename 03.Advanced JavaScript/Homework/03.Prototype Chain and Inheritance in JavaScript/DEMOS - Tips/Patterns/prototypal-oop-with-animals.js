8442ea697e40a09d7b9d97e8582565426b5de06d
Psykepro <nikolai_nedkov@yahoo.com>
2016-02-11 18:48:18 +0200

    Made some changes in the demos patterns

--- 03.Advanced JavaScript/Homework/03.Prototype Chain and Inheritance in JavaScript/DEMOS - Tips/Patterns/prototypal-oop-with-animals.js
+++ 03.Advanced JavaScript/Homework/03.Prototype Chain and Inheritance in JavaScript/DEMOS - Tips/Patterns/prototypal-oop-with-animals.js
@@ -68,4 +68,14 @@ crusher.produceSound();
 crusher.jump();
 crusher.sleep();
 
+// Use this for extending if u need to extend methods \\
+Object.prototype.extend = function (properties) {
+    function f() { };
+    f.prototype = Object.create(this);
+    for (var prop in properties) {
+        f.prototype[prop] = properties[prop];
+    }
+    f.prototype._super = this;
+    return new f();
+};
 
