ce8c2685dbd8ddbd3be16236a26306b93df40fa4
Psykepro <nikolai_nedkov@yahoo.com>
2016-04-17 15:32:31 +0300

    Registration validation fixed

--- 05.JavaScript Frameworks (Angular JS)/Personal Project/Angular-Project-March-2016/IssueTrackingSystem/app/home/unregisteredHome/home-controller.js
+++ 05.JavaScript Frameworks (Angular JS)/Personal Project/Angular-Project-March-2016/IssueTrackingSystem/app/home/unregisteredHome/home-controller.js
@@ -1,6 +1,7 @@
 'use strict';
 
-angular.module('issueTrackingSystem.home', [])
+angular
+    .module('issueTrackingSystem.home', [])
     .controller('HomeCtrl', [
         '$scope',
         '$rootScope',
