4a42e3690ef8c82b81eb2134208ce17953a59cd5
Psykepro <nikolai_nedkov@yahoo.com>
2016-05-07 00:54:17 +0300

    fixed nav-header style

--- 05.JavaScript Frameworks (Angular JS)/Personal Project/Angular-Project-March-2016/IssueTrackingSystem/app/common/header-script.js
+++ 05.JavaScript Frameworks (Angular JS)/Personal Project/Angular-Project-March-2016/IssueTrackingSystem/app/common/header-script.js
@@ -12,34 +12,6 @@
       return this.each(function() {
         navHeader.find('li ul').parent().addClass('has-sub');
         if (settings.format != 'select') {
-          navHeader.prepend('<div id="menu-button">' + settings.title + '</div>');
-          $(this).find("#menu-button").on('click', function(){
-            $(this).toggleClass('menu-opened');
-            var mainmenu = $(this).next('ul');
-            if (mainmenu.hasClass('open')) { 
-              mainmenu.hide().removeClass('open');
-            }
-            else {
-              mainmenu.show().addClass('open');
-              if (settings.format === "dropdown") {
-                mainmenu.find('ul').show();
-              }
-            }
-          });
-
-          multiTg = function() {
-            navHeader.find(".has-sub").prepend('<span class="submenu-button"></span>');
-            navHeader.find('.submenu-button').on('click', function() {
-              $(this).toggleClass('submenu-opened');
-              if ($(this).siblings('ul').hasClass('open')) {
-                $(this).siblings('ul').removeClass('open').hide();
-              }
-              else {
-                $(this).siblings('ul').addClass('open').show();
-              }
-            });
-          };
-
           if (settings.format === 'multitoggle') multiTg();
           else navHeader.addClass('dropdown');
         }
